5c8559bca63eefbf42ae0624705af3092e8ccbd3
hjun813 <hyeonjun0899@google.com>
2025-10-19 16:31:23 +0900

    fix : 에러 출력 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,1 +1,21 @@
-package calculator;import camp.nextstep.edu.missionutils.Console; // 입력import java.util.*;public class Application {    public static void main(String[] args) {        System.out.println("덧셈할 문자열을 입력해 주세요.");        String input = Console.readLine();        calculator calculator = new calculator();        try {            int result = calculator.add(input);            System.out.println("결과 : " + result);        } catch (IllegalArgumentException e) {            System.err.println("오류: " + e.getMessage());        }    }}
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console; // 입력
+import java.util.*;
+
+
+public class Application {
+    public static void main(String[] args) {
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        calculator calculator = new calculator();
+        try {
+            int result = calculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("오류");
+        }
+    }
+}
